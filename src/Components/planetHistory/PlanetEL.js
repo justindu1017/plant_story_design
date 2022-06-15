49d7d79c8ff04c4c58563828597419941db1c4d2
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2022-06-15 21:31:36 +0800

    testing

--- src/Components/planetHistory/PlanetEL.js
+++ src/Components/planetHistory/PlanetEL.js
@@ -6,7 +6,7 @@ export default class PlanetEL extends Component {
   render() {
     return (
       <div
-        className="flex-row-item d-flex my-2 justify-content-center "
+        className="flex-row-item d-flex my-2 justify-content-center  w-33"
         data-bs-toggle="modal"
         data-bs-target="#staticBackdrop"
         data-bs-whatever={this.props.id}
